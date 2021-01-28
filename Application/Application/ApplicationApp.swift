7f6561b604209566702b670b7030e65b4bf30a24
Alex-like <62363788+Alex-like@users.noreply.github.com>
2021-01-28 16:29:48 +0300

    Add SwiftUI
    Simple

---
+++ Application/Application/ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApplicationApp.swift
+//  Application
+//
+//  Created by Alex Shchelochkov on 28.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WeekView()
+        }
+    }
+}
