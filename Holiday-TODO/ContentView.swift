8e01fe93f4550b51f4ca76e528445d41df81b4d0
Karile Myrr <kirillant2013@gmail.com>
2021-01-28 15:11:55 +0300

    Initial Commit

---
+++ Holiday-TODO/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Holiday-TODO
+//
+//  Created by Kariel Myrr on 28.01.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Holiday-TODO/Holiday_TODOApp.swift
@@ -0,0 +1,17 @@
+//
+//  Holiday_TODOApp.swift
+//  Holiday-TODO
+//
+//  Created by Kariel Myrr on 28.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct Holiday_TODOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
